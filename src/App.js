64474aab495eba2d720706169ec92afe058cea04
Abdul Hannan <abdulhani232@gmail.com>
2021-09-04 15:31:17 +0500

    created useEffect and fetchAPI program

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect( ()=>{
+    const getData = async () => {
+      const response = await fetch("https://api.github.com/users/abdul-hannan232/repos")
+      const value = await response.json();
+
+      console.log(value, "value");
+      setData(value);
+    }
+
+    getData();
+
+
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // .then( (response)=> response.json() )
+    // .then ( response => {
+    //   console.log(response, "response");
+    //   setThen(response);
+    // } )
+
+
+  }, [] );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello World</h1>
+
+        <ul>
+
+          {data.map( (repos, index) => {
+            
+            return <li key={index}> {repos.name}  </li>
+
+          })}
+
+        </ul>
+
     </div>
   );
 }
